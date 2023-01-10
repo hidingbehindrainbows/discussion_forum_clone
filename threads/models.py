d9d1c242a66c1d4e793f53257d1e15a6bf6cb742
hidingbehindrainbows <vrishankjvasist@gmail.com>
2023-01-10 21:46:04 +0530

    like, dislike, comment and thread models

--- threads/models.py
+++ threads/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
-from django.contrib.contenttypes.fields import GenericRelation
+# from django.contrib.contenttypes.fields import GenericRelation
 # from django.contrib.auth.models import User
 
 
